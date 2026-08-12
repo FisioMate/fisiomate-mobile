32327802b24c65eff9184fd5468295859d77c549
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2026-08-12 11:12:24 +0700

    build: initialize new demo build separate from the original app
    - Change namespace, applicationId, and label in the Android build
    - Change product bundle id and display name in iOS

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.fisiomate"
+    namespace = "com.example.fisiomate.demo"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.fisiomate"
+        applicationId = "com.example.fisiomate.demo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
